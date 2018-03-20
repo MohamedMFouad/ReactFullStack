990773f8c111a4514463a9433a9f58470150ee56
Mohamed.M.Fouad <Mohamed.M.Fouad.Mahmoud@gmail.com>
2018-03-20 13:54:48 +0200

    sass translate

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,29 @@
-import Header from './Header'
-import React from'react'
+import React from 'react';
+import Header from './Header';
+import ContestsPreview from './ContestsPreview'
 
- 
-class App extends  React.Component{
-    state ={
-        pageHeader :'React'
-    }
-   componentDidMount(){
-       console.log('CDM');
-       debugger;
-   }
-   componentWillUnmount(){
-       console.log("unmounted");
-       debugger;
-   }
-   
-    render(){
-        return(
-            <div className="App">
-              <Header massage={this.state.pageHeader} />
-                <div>
-                 ...---
-               </div>
-             </div>  
-         );
-    }
-};
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  componentDidMount() {
+    // timers, listeners
+  }
+  componentWillUnmount() {
+    // clean timers, listeners
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.props.contests.map(contest =>
+            <ContestsPreview {...contest} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
-
+import data from './testData.json'
 
 ReactDOM.render(
-<App headerMassage="dsadasd"/> ,
+<App contests={data.contests}/> ,
   document.getElementById('root')
 );
-setTimeout(()=>{
-  ReactDOM.render(
-    <h3>sdasd</h3>,
-    document.getElementById('root')
-  )
-},4000);
+// setTimeout(()=>{
+//   ReactDOM.render(
+//     <h3>sdasd</h3>,
+//     document.getElementById('root')
+//   )
+// },4000);
